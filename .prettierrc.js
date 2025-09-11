41a51ff677dc82393fd4316c5d3f1ac9c8ad9be4
Huang Yi-Lun <yilunh5@gmail.com>
2025-09-11 12:10:57 +0800

    重新上傳所有檔案#3

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+ module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  plugins: ['prettier-plugin-tailwindcss'], // 自動排序 Tailwind classes
+}
